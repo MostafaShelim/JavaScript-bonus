bb50de9c34963d7968f57b9e05878ad408a0d9b5
MostafaShelim <mostafarmstu@gmail.com>
2020-09-02 18:10:09 +0600

    Immdiately invoked function expresion

--- IIFE.js
+++ IIFE.js
@@ -2,3 +2,12 @@
     const romancticName = "morhi amin";
     console.log(romancticName);
 })();
+
+function toDo(){
+    const newTaskElement = document.createElement('li');
+}//function declaration
+
+var result = function kikorbo(){
+    console.log("asole ki korlam");
+}
+console.log(result);//function expression
